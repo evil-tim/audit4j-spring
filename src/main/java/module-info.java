fd4b8b6d83f7d58cdc0b613dce6fafe4873052fe
James Villegas <evil-tim@users.noreply.github.com>
2022-06-19 17:52:50 +0800

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module audit4j.spring {
+    requires aspectjrt;
+    requires audit4j.core;
+    requires javax.servlet.api;
+    requires spring.aop;
+    requires spring.beans;
+    requires spring.security.core;
+    requires spring.security.web;
+    requires spring.web;
+
+    exports org.audit4j.integration.spring;
+}
