d4f09c021e0b3041ef1ea11482f717661b67c53e
James Villegas <evil-tim@users.noreply.github.com>
2022-06-26 12:16:07 +0800

    Cleanup unused annotations

--- src/main/java/org/audit4j/integration/spring/WebSessionAuditMetaData.java
+++ src/main/java/org/audit4j/integration/spring/WebSessionAuditMetaData.java
@@ -17,19 +17,17 @@
  */
 package org.audit4j.integration.spring;
 
-import org.audit4j.core.util.annotation.Beeta;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
 /**
  * Default Metadata class to capture actor and origin information from spring
  * application context using session attributes and servlet requests.
- * 
+ *
  * @author <a href="mailto:janith3000@gmail.com">Janith Bandara</a>
- * 
+ *
  * @since 2.4.1
  */
-@Beeta
 public class WebSessionAuditMetaData extends SpringWebAuditMetadata {
 
     /** The Constant serialVersionUID. */
@@ -40,14 +38,14 @@ public class WebSessionAuditMetaData extends SpringWebAuditMetadata {
 
     /**
      * {@inheritDoc}
-     * 
+     *
      * @see org.audit4j.core.MetaData#getActor()
-     * 
+     *
      */
     @Override
     public String getActor() {
-        String actor = (String) RequestContextHolder.currentRequestAttributes()
-                .getAttribute(actorSessionAttribute, RequestAttributes.SCOPE_SESSION);
+        String actor = (String) RequestContextHolder.currentRequestAttributes().getAttribute(actorSessionAttribute,
+            RequestAttributes.SCOPE_SESSION);
         if (actor != null) {
             return actor;
         }
@@ -56,9 +54,8 @@ public class WebSessionAuditMetaData extends SpringWebAuditMetadata {
 
     /**
      * Sets the actor session attribute.
-     * 
-     * @param actorSessionAttribute
-     *            the new actor session attribute
+     *
+     * @param actorSessionAttribute the new actor session attribute
      */
     public void setActorSessionAttribute(String actorSessionAttribute) {
         this.actorSessionAttribute = actorSessionAttribute;
